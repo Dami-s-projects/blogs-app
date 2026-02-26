4535877485b578035d2bbf165339f82ce17ec9d7
Damilola <damilolaabiolasa@gmail.com>
2026-02-26 13:47:18 +0000

    Configure settings for Railway deployment
    Initial changes. If this works. I wont make any other changes
    to settings.py for now. These changes are made so deployment
    to Railway can be successful.

--- Blog/settings.py
+++ Blog/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+import os
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lk5a2-l5sma#-_m&0gmj8dp62v%4)1cevs_8$=$ofp5(8@r6g@'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-lk5a2-l5sma#-_m&0gmj8dp62v%4)1cevs_8$=$ofp5(8@r6g@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -123,3 +126,6 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'blogs:index'
 LOGOUT_REDIRECT_URL = 'blogs:index'
 LOGIN_URL = 'accounts:login'
+
+#static root for railway deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
