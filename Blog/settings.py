a743d58b671ac0d384097aa7977cac44facf9deb
Damilola <damilolaabiolasa@gmail.com>
2026-02-26 17:38:20 +0000

    Added CSRF_TRUSTED_ORIGINS variable
    All url paths in this variable will be allowed and trusted
    
    I will customize the url that will be allowed later on the Railway interface

--- Blog/settings.py
+++ Blog/settings.py
@@ -32,6 +32,10 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost:8000,http://127.0.0.1:8000'
+).split(',')
 
 # Application definition
 
